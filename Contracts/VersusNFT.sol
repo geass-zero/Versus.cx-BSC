ea995daad5cbdf999cc81c9b16ce5b59e7598396
geass-zero <nyanfinance@gmail.com>
2021-05-17 23:13:50 -0400

    NFT edit

--- Contracts/VersusNFT.sol
+++ Contracts/VersusNFT.sol
@@ -6,45 +6,43 @@ contract VersusNFT is BEP721 {
     using SafeMath for uint256;
     address public owner;
     
-    uint256 _tokenIds;
+    uint256 _tokenId;
     
     struct details {
-        address originalOwner;
-        string tier;
+        uint32 NFTLevel;
         uint112 bonus;
         bool isStaked;
     }
     mapping(uint256 => details) nftDetails; 
 
     constructor() public payable BEP721("Versus Badge", "VersusNFT") {
+        owner = msg.sender;
 
     }
-    
-    function NFTConstructor() public {
-        require(!initialized);
-        owner = msg.sender;
-        constructor1("Versus Bagde", "VersusNFT");
-        initialize();
-    }
+
 
     function setOwner(address _owner) public _onlyOwner delegatedOnly {
         owner = _owner;
     }
+
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        //use tokenId to determine NFT level
+        uint256 tokenLevel = nftDetails[tokenId].NFTLevel;
+        return super.tokenURI(tokenLevel);
+    }
     
     /** @notice Creates a new Versus NFT.
       * @param _claimer Address of the claiming user.
-      * @param _tier Name of the NFT tier.
+      * @param _level Name of the NFT tier.
       */
-    function createNFT(address _claimer, string memory _tier) public delegatedOnly returns(uint256) {
+    function createNFT(address _claimer, uint256 _level) public delegatedOnly returns(bool) {
+        require(msg.sender == versusToken);
         uint256 newItemId = _tokenIds.add(1);
         _tokenIds = _tokenIds.add(1);
         _mint(_claimer, newItemId);
         nftDetails[newItemId].originalOwner = _claimer;
-        nftDetails[newItemId].tier = _tier;
-        if (keccak256(bytes(_tier)) == "COMMON") {
-            nftDetails[newItemId].bonus = 1;
-            _setTokenURI(newItemId, "https://ipfs.io/ipfs/QmQLNaJ6mW8u63j2GzXujffMtGtTSwBZQFUoP2P5BmjZ2b");
-        }
+        nftDetails[newItemId].level = _level;
         
         return newItemId;
     }
